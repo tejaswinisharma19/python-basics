e1cf8d81ef06f62b89c8ec920c1f22a8587f983d
tejaswinisharma19 <tejaswinisharma1919@gmail.com>
2026-09-08 10:53:08 +0530

    Add employee performance rating

---
+++ day-02/01_performance_rating.py
@@ -0,0 +1,17 @@
+score = int(input())
+
+if score  >=  90:
+    print("Excellent")
+
+elif score >= 75:
+    print("Good")
+
+elif score >= 60:
+    print("Satisfactory")
+
+elif score >= 40:
+    print("Needs Improvement")
+    
+
+else:
+    print("Poor")
