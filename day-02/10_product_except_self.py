a30cae403abd49b0a241def664504c512ec65d03
tejaswinisharma19 <tejaswinisharma1919@gmail.com>
2026-09-08 17:45:49 +0530

    Add product except self solution

---
+++ day-02/10_product_except_self.py
@@ -0,0 +1,14 @@
+nums = [1, 2, 3, 4]
+
+answer = []
+
+for i in range(len(nums)):
+    product = 1
+
+    for j in range(len(nums)):
+        if i != j:
+            product = product * nums[j]
+
+    answer.append(product)
+
+print(answer)
